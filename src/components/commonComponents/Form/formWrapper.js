a764bc2c1ee3e1e06383fec20344da23ae48faad
Juanjo4U <juanjos.xf@gmail.com>
2020-03-26 10:07:22 +0100

    shadow box component added

--- src/components/commonComponents/Form/formWrapper.js
+++ src/components/commonComponents/Form/formWrapper.js
@@ -1,21 +1,7 @@
 import React from "react";
-import { Platform } from "react-native";
-import styled from "styled-components/native";
-import { sizeNormalize } from "../../../utils/adaptSizes";
-import constants from "../../../utils/constants";
-
-const Wrapper = styled.View`
-    padding: ${sizeNormalize(12)}px;
-    borderRadius: ${sizeNormalize(10)}px;
-    borderWidth: .25px;
-    elevation: ${sizeNormalize(Platform.OS === 'ios' ? 10 : 6)};
-    shadowColor: gray;
-    shadowOpacity: 0.1;
-    shadowRadius: ${sizeNormalize(5)}px;
-    backgroundColor: ${constants.secondaryColor};
-`;
+import Wrapper from "../../commonComponents/Wrappers/ShadowBox";
 
 export default ({ children }) =>
-    <Wrapper style={{ shadowOffset: { width: -1, height: 10 } }}>
+    <Wrapper>
         {children}
     </Wrapper>
