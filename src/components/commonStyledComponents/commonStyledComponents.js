91aab2f87e2a40cb0ba19dc2293ca736cf0ae2bd
Juanjo4U <juanjos.xf@gmail.com>
2020-04-02 11:22:04 +0200

    safeArea added to commonStyledComponents

--- src/components/commonStyledComponents/commonStyledComponents.js
+++ src/components/commonStyledComponents/commonStyledComponents.js
@@ -1,6 +1,40 @@
+import { Platform, SafeAreaView } from "react-native";
+import { SafeAreaView as SafeAreaViewAndroid } from "react-native-safe-area-context";
 import { sizeNormalize } from "../../utils/adaptSizes";
 import styled from "styled-components/native";
 import constants from "../../utils/constants";
+
+export const SafeArea = styled(Platform.OS === 'ios' ? SafeAreaView : SafeAreaViewAndroid)`
+    flex: ${({ f }) => f || 1};
+    ${({ br }) => br && `borderRadius: 10px;`}
+    ${({ m }) => m && `margin: ${sizeNormalize(m)}px;`}
+    ${({ mh }) => mh && `marginHorizontal: ${sizeNormalize(mh)}px;`}
+    ${({ mv }) => mv && `marginVertical: ${sizeNormalize(mv)}px;`}
+    ${({ p }) => p && `padding: ${p}px;`}
+    ${({ ph }) => ph && `paddingHorizontal: ${sizeNormalize(ph)}px;`}
+    ${({ pv }) => pv && `paddingVertical: ${sizeNormalize(pv)}px;`}
+    ${({ bg }) => bg && `backgroundColor: ${bg};`}
+    ${({ center }) => center && `justifyContent: center; alignItems: center;`}
+    position: relative;
+    ${({ justifyCenter }) => justifyCenter && `justifyContent: center;`}
+    ${({ alignCenter }) => alignCenter && `alignItems: center;`}
+`;
+
+export const Content = styled.View`
+    flex: ${({ f }) => f || 1};
+    ${({ br }) => br && `borderRadius: 10px;`}
+    ${({ m }) => m && `margin: ${sizeNormalize(m)}px;`}
+    ${({ mh }) => mh && `marginHorizontal: ${sizeNormalize(mh)}px;`}
+    ${({ mv }) => mv && `marginVertical: ${sizeNormalize(mv)}px;`}
+    ${({ p }) => p && `padding: ${p}px;`}
+    ${({ ph }) => ph && `paddingHorizontal: ${sizeNormalize(ph)}px;`}
+    ${({ pv }) => pv && `paddingVertical: ${sizeNormalize(pv)}px;`}
+    ${({ bg }) => bg && `backgroundColor: ${bg};`}
+    ${({ center }) => center && `justifyContent: center; alignItems: center;`}
+    position: relative;
+    ${({ justifyCenter }) => justifyCenter && `justifyContent: center;`}
+    ${({ alignCenter }) => alignCenter && `alignItems: center;`}
+`;
 
 export const Label = styled.Text`
     color: ${({ c }) => c || constants.textDefaultColor};
@@ -18,20 +52,4 @@ export const Label = styled.Text`
         textDecorationStyle: solid;
         textDecorationColor: ${c || constants.textDefaultColor};
     `}
-`;
-
-export const Content = styled.View`
-    flex: ${({ f }) => f || 1};
-    ${({ br }) => br && `borderRadius: 10px;`}
-    ${({ m }) => m && `margin: ${sizeNormalize(m)}px;`}
-    ${({ mh }) => mh && `marginHorizontal: ${sizeNormalize(mh)}px;`}
-    ${({ mv }) => mv && `marginVertical: ${sizeNormalize(mv)}px;`}
-    ${({ p }) => p && `padding: ${p}px;`}
-    ${({ ph }) => ph && `paddingHorizontal: ${sizeNormalize(ph)}px;`}
-    ${({ pv }) => pv && `paddingVertical: ${sizeNormalize(pv)}px;`}
-    ${({ bg }) => bg && `backgroundColor: ${bg};`}
-    ${({ center }) => center && `justifyContent: center; alignItems: center;`}
-    position: relative;
-    ${({ justifyCenter }) => justifyCenter && `justifyContent: center;`}
-    ${({ alignCenter }) => alignCenter && `alignItems: center;`}
 `;
--- src/screens/record/record.js
+++ src/screens/record/record.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { Platform, SafeAreaView } from "react-native";
-import { SafeAreaView as SafeAreaViewAndroid } from "react-native-safe-area-context";
-import styled from "styled-components/native";
+import { SafeArea as Wrapper } from "../../components/commonStyledComponents/commonStyledComponents";
 import RecordList from "../../components/record/recordList";
 import constants from "../../utils/constants";
 
-const Wrapper = styled(Platform.OS === 'ios' ? SafeAreaView : SafeAreaViewAndroid)`
-    flex: ${({ f }) => f || 1};
-    backgroundColor: ${({ bg }) => bg || constants.backgroundColor};
-`;
-
 export default () =>
-    <Wrapper>
+    <Wrapper bg={constants.backgroundColor}>
         <RecordList />
     </Wrapper>
