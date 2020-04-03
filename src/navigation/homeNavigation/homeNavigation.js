95d5eda4004c1fc018b707f29c61a18de2571157
Juanjo4U <juanjos.xf@gmail.com>
2020-04-03 16:39:54 +0200

    added safeArea for small header

--- src/navigation/homeNavigation/homeNavigation.js
+++ src/navigation/homeNavigation/homeNavigation.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Header from "../navgationComponents/header/header";
 import customizeGoal from "../../screens/customizeGoal/customizeGoal";
 import Bottom from "../bottomNavigation/bottomNavigation";
-import { Content as Wrapper } from "../../components/commonStyledComponents/commonStyledComponents";
+import { SafeArea as Wrapper } from "../../components/commonStyledComponents/commonStyledComponents";
 import constants from "../../utils/constants";
 
 const WithoutHeader = () => <Wrapper bg={constants.primaryColor} />
