cb8ca106ea469d51c626578efe98a940d738387c
Juanjo4U <juanjos.xf@gmail.com>
2020-04-09 12:54:33 +0200

    logo white added to login

--- src/components/auth/login/loginBackground.js
+++ src/components/auth/login/loginBackground.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Label, Content } from "../../commonStyledComponents/commonStyledComponents";
+import { logoWhite as logo } from "../../../assets/logo/logo";
 import Wrapper from "../authBackWrapper";
 import constants from "../../../utils/constants";
+import styled from "styled-components/native";
+
+export const Logo = styled.Image`
+
+`;
 
 export default ({ }) =>
     <Wrapper color={constants.primaryColor}>
         <Content f={.5} center>
-            <Label c={constants.textSecondaryColor} fw={900} fs={50} >
-                Trainerer
-            </Label>
+            <Logo source={logo} />
 
             <Label c={constants.textSecondaryColor} fs={22} center m={40}>
                 Si tienes un objetivo, aquí está tu plan de entrenamiento
